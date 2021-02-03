d5f66f92ed6f31716f7afee7aa9c52320c758b14
zrfan <1522193661@qq.com>
2021-02-03 23:59:55 +0800

    Fx

--- models/bert_models/bert_model.py
+++ models/bert_models/bert_model.py
@@ -19,8 +19,24 @@ def checkModelGraph(path):
         saver.restore(sess, path + "/albert_model/albert_model.ckpt")
         graph = tf.get_default_graph()  # 获取当前图，为了后续训练时恢复变量
         tensor_name_list = [tensor.name for tensor in graph.as_graph_def().node] # 得到当前图中所有变量的名称
-        for k in tensor_name_list:
-            print("get variable:", k)
+        # for k in tensor_name_list:
+        #     print("get variable:", k)
+        print("all var len=", len(tensor_name_list))
+        tokens = [1, 4, 7, 23, 5, 3]   # ["[CLS]", "我", "爱", "中", "国", "[SEP]"]
+        seg_ids = [0, 0, 0, 0, 0, 0]
+        mask = [1] * len(tokens)
+        max_seq_length = 128
+        while len(seg_ids) < max_seq_length:
+            tokens.append(0)
+            seg_ids.append(0)
+            mask.append(0)
+        # model inputs: input_ids , input_mask, segment_ids, label_id
+        input_ids = graph.get_tensor_by_name("input_ids")
+        input_mask = graph.get_tensor_by_name("input_mask")
+        segment_ids = graph.get_tensor_by_name("segment_ids")
+        output = graph.get_tensor_by_name("cls/seq_relationship/Mean")
+        result = sess.run(output, feed_dict={input_ids: tokens, input_mask: mask, segment_ids: seg_ids})
+        print("result=", result)
 
 
 if __name__ == "__main__":
