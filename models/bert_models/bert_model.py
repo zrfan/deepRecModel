27365beea69c7b0d5d53c99fab05ceb06c1ae5ce
zrfan <1522193661@qq.com>
2021-02-03 23:31:16 +0800

    Fx

--- models/bert_models/bert_model.py
+++ models/bert_models/bert_model.py
@@ -6,7 +6,9 @@ def checkModel(path):
     print("all var below:")
     chkp.print_tensors_in_checkpoint_file(path+"/albert_model/albert_model.ckpt", tensor_name="", all_tensors=True)
 
+#     cls/predictions/transform/dense/bias
 
+    chkp.print_tensors_in_checkpoint_file(path + "/albert_model/albert_model.ckpt", tensor_name="cls/predictions/transform/dense/bias", all_tensors=True)
 
 if __name__ == "__main__":
     checkModel("../../data/pretrain_model/")
