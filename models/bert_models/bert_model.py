4d8610d53a861c24198513b4c2dd98e9d4f8ce9b
zrfan <1522193661@qq.com>
2021-02-03 23:29:18 +0800

    Fx

--- models/bert_models/bert_model.py
+++ models/bert_models/bert_model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
 def checkModel(path):
-    var = chkp.print_tensors_in_checkpoint_file(path+"/albert_model.ckpt", tensor_name="", all_tensors=True)
+    var = chkp.print_tensors_in_checkpoint_file(path+"/albert_model/albert_model.ckpt", tensor_name="", all_tensors=True)
 
     print("all var below:")
     print(var)
