52cf110bef4ad1921e7886ed5ed57fc3d0d568c1
zhongrui3 <zhongrui3@staff.weibo.com>
2021-02-04 11:16:20 +0800

    fx

--- models/bert_models/bert_model.py
+++ models/bert_models/bert_model.py
@@ -41,9 +41,9 @@ def checkModelGraph(path):
         result = sess.run(output, feed_dict={input_ids: [tokens], input_mask: [mask], segment_ids: seg_ids})
         print("result=", result)
         #savedmodel文件保存
-　　    builder = tf.saved_model.builder.SavedModelBuilder(path+'/saved_bert_model/')
-　　    builder.add_meta_graph_and_variables(sess)
-　　    builder.save()
+        builder = tf.saved_model.builder.SavedModelBuilder(path+'/saved_bert_model/')
+　　     builder.add_meta_graph_and_variables(sess)
+　　     builder.save()
 
         # constant_graph = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def)
         # with tf.gfile.FastGFile(path+'/test_model.pb', mode='wb') as f:
