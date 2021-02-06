bda31c6efe5409ca83a408a8f89d353929aba728
zhongrui3 <zhongrui3@staff.weibo.com>
2021-02-06 17:51:25 +0800

    fx LR

--- data/downloadData.sh
+++ data/downloadData.sh
@@ -4,6 +4,7 @@
 # movielens ml-25m dataset
 wget http://files.grouplens.org/datasets/movielens/ml-25m.zip
 wget http://files.grouplens.org/datasets/movielens/ml-1m.zip
+python 1m_data_process.py
 
 
 # ESSM public dataset, Alibaba click and conversion prediction
