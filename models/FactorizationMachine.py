89fe1f573ec3a451b70bd39a52213aa684a522c1
zhongrui3 <zhongrui3@staff.weibo.com>
2021-02-08 15:35:29 +0800

    Fx

--- models/FactorizationMachine.py
+++ models/FactorizationMachine.py
@@ -6,10 +6,12 @@ from numpy import *
 from random import normalvariate  # 正态分布
 from sklearn import preprocessing
 import numpy as np
+from data_util import get1MTrainData
 
 class FM(object):
-    def __init__(self):
-        self.data, self.feature_potential, self.iter = None, None, None
+    def __init__(self, data_path, iter, task_type, k=8):
+        self.data_path, self.feature_potential, self.iter = data_path, k, iter
+        self.task_type = task_type  # 0: classification 1: regression
         self.alpha, self._w, self._w_0, self.v = None, None, None, None
         self.with_col, self.first_col = None, None
     def sigmoid(self, inx):
@@ -17,32 +19,41 @@ class FM(object):
     def fit(self, data, feature_potential=8, alpha=0.01, iter=100):
         # alpha 是学习速率
         self.alpha, self.iter, self.feature_potential = alpha, iter, feature_potential
-        dataMatrix, classLabels = self.loadDataSet(data)
+        user_info, movie_info, rating_info, user_cols, movie_cols = get1MTrainData(self.data_path)
         k = self.feature_potential
-        m, n = shape(dataMatrix)
+        m, n = len(rating_info), len(user_cols)+len(movie_cols)-2
         # 初始化参数
         w = np.zeros((n, 1))  # n是特征的个数
         w_0 = 0
         v = normalvariate(0, 0.2) * np.ones((n, k))
         for it in range(self.iter):
-            for x  in range(m):
+            for idx, row  in rating_info.iterrows():
+                userId, itemId = int(row["userId"]), int(row["movieId"])
+                userInfo, movieInfo = userData.loc[userId, :], itemData.loc[itemId, :]
+                trainData = userInfo.tolist() + movieInfo.tolist()
+                x = np.asarray(trainData)
+                y = float(row["ratings"])/5
                 # 对应点积的地方通常会有sum，对应位置积的地方通常没有
-                inter_1 = dataMatrix[x] * v   # xi * vi, xi与vi的矩阵点积
+                inter_1 = x * v   # xi * vi, xi与vi的矩阵点积
+                print("inter_1=", inter_1)
                 # xi与xi的对应位置乘积 与 xi^2与vi^2对应位置的乘积的点积，
-                inter_2 = np.multiply(dataMatrix[x], dataMatrix[x]) * np.multiply(v, v)    # multiply对应元素相乘
+                inter_2 = np.multiply(x, x) * np.multiply(v, v)    # multiply对应元素相乘
+                print("inter_2=", inter_2)
                 # 完成交叉项 xi*vi*xi*vi - xi^2*vi^2
                 interaction = np.sum(np.multiply(inter_1, inter_1) - inter_2) / 2
+                print("interaction=", interaction)
                 # 计算预测的输出
-                p = w_0 + dataMatrix[x]*w + interaction
+                p = w_0 + x*w + interaction
+                print("p=", p)
                 # 计算sigmoid（y*pred_y）-1
-                loss = self.sigmoid(classLabels[x] * p[0, 0]) -1
+                loss = (self.sigmoid(y * p[0, 0]) -1)*y if self.task_type == 0 else self.sigmoid(p[0, 0])-y
                 # 更新参数
-                w_0 = w_0 - self.alpha * loss * classLabels[x]
+                w_0 = w_0 - self.alpha * loss
                 for i in range(n):
-                    if dataMatrix[x, i] != 0:
-                        w[i, 0] = w[i, 0] - self.alpha * loss * classLabels[x] * dataMatrix[x, i]
+                    if x[i] != 0:
+                        w[i, 0] = w[i, 0] - self.alpha * loss * x[i]
                         for j in range(k):
-                            v[i, j] = v[i, j] - self.alpha * loss * classLabels[x] * (dataMatrix[x, i] * inter_1[0, j] - v[i,j]*dataMatrix[x, i]*dataMatrix[x, i])
+                            v[i, j] = v[i, j] - self.alpha * loss * (x[i] * inter_1[0, j] - v[i,j]*x[i]*x[i])
         
         self._w_0, self._w, self._v = w_0, w, w
     def predict(self, data):
@@ -62,5 +73,6 @@ class FM(object):
     # def loadDataSet(self, data, with_col=True, first_col=2):
 
 if __name__=='__main__':
-    fm = FM()
+    fm = FM(data_path="../data/ml-1m/", iter=10, task_type=1)
+    fm.fit()
 
---
+++ models/data_util.py
@@ -0,0 +1,92 @@
+#-*- coding: utf-8 -*
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from time import time
+import math
+import csv
+# from data_process import getYear
+
+def getYear(x):
+    arr = x.split("(")
+    if len(arr) == 1:
+        return 0
+    y = 0
+    for t in arr:
+        try:
+            y = int(t[0:4])
+        except:
+            continue
+    return y
+
+def getTrainData(path):
+    movie_info = pd.read_csv(path + "/all_movie_info.csv", sep=",", names=["movieId", "movie_title", "genres", "year", "all_tag"], skiprows=1)
+    # user_info = pd.read_csv(path + "/all_users.csv", sep=",", names=["maxTime", "minTime", "tagCount", "userId", "dur_day", "avg_day_movie"], skiprows=1)
+    # rating = pd.read_csv(path + "/ratings.csv", names=["userId", "movieId", "rating", "timestamp"], skiprows=1, nrows=1000)
+
+    all_genres = pd.read_csv(path+"/../all_genres.csv", sep=",", names=["genres"])["genres"].tolist()
+    years = pd.read_csv(path+"/../all_year.csv", sep=",", names=["year"])["year"].tolist()
+    for genres in all_genres:
+        movie_info[genres] = 0
+    for year in years:
+        movie_info[year] = 0
+    movie_info["movie_tag_count"] = 0
+
+    print(movie_info["all_tag"].head(10))
+    for idx, row in movie_info.iterrows():
+        year = row["year"]
+        row[year] = 1
+        genresStr = row["genres"].split("|")
+        for g in genresStr:
+            row[g] = 1
+        tags = row["all_tag"].split(":")
+        row["movie_tag_count"] = len(tags)
+
+    print(movie_info.head(10))
+
+def get1MTrainData(path):
+    user_info = pd.read_csv(path+"/users.dat", header=None, encoding='utf-8', delimiter="::", quoting=csv.QUOTE_NONE,
+                            names=["userId", "gender", "age", "occupation", "zipcode"])
+    genderList = ["F", "M"]
+    ageList = [1, 18, 25, 35, 45, 50, 56]
+    occupationList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    user_cols = ["gender_"+str(x) for x in genderList] + ["age_"+str(x) for x in ageList] + ["occ_"+str(x) for x in occupationList] + ["userId"]
+    genderInfo = pd.get_dummies(user_info["gender"], sparse=True)
+    ageInfo = pd.get_dummies(user_info["age"], sparse=True)
+    occInfo = pd.get_dummies(user_info["occupation"], sparse=True)
+    occInfo.columns =["occ_"+str(x) for x in occupationList]
+    ageInfo.columns = ["age_"+str(x) for x in ageList]
+    genderInfo.columns = ["gender_"+x for x in genderList]
+    user_info = user_info.join(genderInfo).join(ageInfo).join(occInfo)[user_cols]
+    user_info = user_info.set_index("userId")
+    print(user_info.head(10))
+
+    movie_info = pd.read_csv(path+"/movies.dat", header=None, delimiter="::", quoting=csv.QUOTE_NONE, names=["movieId", "title", "genres"])
+    movie_info["year"] = movie_info["title"].apply(lambda x: getYear(x))
+    genresList = pd.read_csv(path + "/../all_genres.csv", sep=",", names=["genres"])["genres"].tolist()
+    yearList = movie_info["year"].tolist()
+    
+    yearInfo = pd.get_dummies(movie_info["year"], sparse=True)
+    yearInfo.columns = ["year_"+str(x) for x in yearInfo.columns ]
+    print(yearInfo.head(10))
+    movie_info = movie_info.join(yearInfo)
+    print(movie_info.head(10))
+    
+    for g in genresList:
+        movie_info["genres_"+g] = 0
+    for idx, row in movie_info.iterrows():
+        gList = row["genres"].split("|")
+        for g in gList:
+            movie_info.loc[idx, "genres_"+g] = 1
+    movie_cols = ["genres_" + x for x in genresList] + ["year_" + str(x) for x in yearList] + ["movieId"]
+    movie_info = movie_info[movie_cols]
+    print(movie_info.head(10))
+    movie_info = movie_info.set_index("movieId")
+
+    rating_info = pd.read_csv(path+"/ratings.dat", header=None, delimiter="::", quoting=csv.QUOTE_NONE, names=["userId", "movieId", "ratings", "timestamp"])
+    
+    return user_info, movie_info, rating_info, user_cols, movie_cols
+
+def get1MTrainDataWithNeg(path):
+    user_info, movie_info, rating_info, user_cols, movie_cols = get1MTrainData(path)
+    
