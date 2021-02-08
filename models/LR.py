71f7d1ad116ab36cfa8fabbec4e429e485e4194c
zhongrui3 <zhongrui3@staff.weibo.com>
2021-02-08 14:29:04 +0800

    Fx

--- models/LR.py
+++ models/LR.py
@@ -69,8 +69,9 @@ def get1MTrainData(path):
     movie_cols = ["generes_" + x for x in genresList] + ["year_" + str(x) for x in yearList] + ["movieId"]
     yearInfo = pd.get_dummies(movie_info["year"], sparse=True)
     yearInfo.columns = ["year_"+str(x) for x in yearInfo.columns ]
+    print(yearInfo.head(10))
     movie_info = movie_info.join(yearInfo)[movie_cols]
-
+    print(movie_info.head(10))
     movie_info = movie_info.set_index("movieId")
     print(movie_info.head(10))
     for g in genresList:
