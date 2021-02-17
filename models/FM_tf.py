1033142f357218efde0381de9ff49dc7784e6e34
zrfan <1522193661@qq.com>
2021-02-17 19:55:17 +0800

    add

--- models/FM_tf.py
+++ models/FM_tf.py
@@ -39,7 +39,7 @@ class FMModel(object):
         embedding_size, feature_size, field_size = self.params["embedding_size"], self.params["feature_size"], self.params["field_size"]
         batch_size, learning_rate, optimizer_used = self.params["batch_size"], self.params["learning_rate"], self.params["optimizer"]
         feature_idx = features["feature_idx"]
-        feature_idx = tf.reshape(feature_idx, shape=[batch_size, field_size])
+        feature_idx = tf.reshape(feature_idx, shape=[batch_size, tf.shape(feature_idx)[1]])
         labels = tf.reshape(labels, shape=[batch_size, 1])
         feature_values = features["feature_values"]
         feature_values = tf.reshape(feature_values, shape=[batch_size, field_size])
@@ -110,7 +110,7 @@ class FMModel(object):
         dataset = tf.data.Dataset.from_generator(gen, ({"feature_idx":tf.int64, "feature_values":tf.float32}, tf.float32),
                                                  ({"feature_idx":tf.TensorShape([None]), "feature_values":tf.TensorShape([None])},
                                                   tf.TensorShape([])))
-        dataset = dataset.batch(1)
+        dataset = dataset.batch(self.params["batch_size"])
 
         return dataset
     def train(self):
