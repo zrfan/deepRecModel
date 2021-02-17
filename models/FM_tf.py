bfb169191180a4c7c4b1ed25d891fb5c705c0ab8
zrfan <1522193661@qq.com>
2021-02-17 21:39:16 +0800

    add

--- models/FM_tf.py
+++ models/FM_tf.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*
+# -*- coding: utf-8 -*
 # tf1.14
 from __future__ import division
 from math import exp
@@ -12,11 +12,14 @@ import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # 使用第0块GPU
 
+
 # https://zhuanlan.zhihu.com/p/145436595
 class FMModelParams(object):
     """ class for initializing weights"""
+
     def __init__(self, params):
         self.embedding_size, self.feature_size = params["embedding_size"], params["feature_size"]
+
     def initialize_weights(self):
         """ init fm weights
         returns
@@ -27,20 +30,28 @@ class FMModelParams(object):
         weights = dict()
         weights_initializer = tf.glorot_normal_initializer()
         bias_initializer = tf.constant_initializer(0.0)
-        weights["feature_embeddings"] = tf.get_variable(name="weights", dtype=tf.float32, initializer=weights_initializer,
-                                                        shape=[self.feature_size+1, self.embedding_size])
-        weights["weights_first_order"] = tf.get_variable(name="vector", dtype=tf.float32, initializer=weights_initializer,
-                                                         shape=[self.feature_size+1, 1])
+        weights["feature_embeddings"] = tf.get_variable(name="weights", dtype=tf.float32,
+                                                        initializer=weights_initializer,
+                                                        shape=[self.feature_size + 1, self.embedding_size])
+        weights["weights_first_order"] = tf.get_variable(name="vector", dtype=tf.float32,
+                                                         initializer=weights_initializer,
+                                                         shape=[self.feature_size + 1, 1])
         weights["fm_bias"] = tf.get_variable(name="bias", dtype=tf.float32, initializer=bias_initializer, shape=[1])
         return weights
+
+
 class FMModel(object):
     """ FM implementation for tensorflow"""
+
     def __init__(self, data_path, params):
         self.data_path, self.params = data_path, params
+
     def fm_model_fn(self, features, labels, mode):
         """ build tf model """
-        embedding_size, feature_size, field_size = self.params["embedding_size"], self.params["feature_size"], self.params["field_size"]
-        batch_size, learning_rate, optimizer_used = self.params["batch_size"], self.params["learning_rate"], self.params["optimizer"]
+        embedding_size, feature_size, field_size = self.params["embedding_size"], self.params["feature_size"], \
+                                                   self.params["field_size"]
+        batch_size, learning_rate, optimizer_used = self.params["batch_size"], self.params["learning_rate"], \
+                                                    self.params["optimizer"]
         feature_idx = features["feature_idx"]
         print("feature  :   ########################3\n", features)
         feature_idx = tf.reshape(feature_idx, shape=[batch_size, tf.shape(feature_idx)[1]])
@@ -95,29 +106,56 @@ class FMModel(object):
         predictions = {"prob": predicts}
         return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.TRAIN, predictions=predicts, loss=loss,
                                           eval_metric_ops=eval_metric_ops, train_op=train_op)
+
     def train_input_fn(self):
-        # tf.compat.v1.enable_eager_execution()
         userData, itemData, rating_info, user_cols, movie_cols = get1MTrainData(self.data_path)
-        self.params["feature_size"] = len(user_cols)+len(movie_cols)-2
+        self.params["feature_size"] = len(user_cols) + len(movie_cols) - 2
+
         def gen():
             for idx, row in rating_info.iterrows():
                 userId, itemId = int(row["userId"]), int(row["movieId"])
                 userInfo, movieInfo = userData.loc[userId, :], itemData.loc[itemId, :]
                 trainData = userInfo.tolist() + movieInfo.tolist()
-                feature_index = list(filter(lambda x: x[0]==1, zip(trainData, list(range(1, len(trainData)+1)))))
+                feature_index = list(filter(lambda x: x[0] == 1, zip(trainData, list(range(1, len(trainData) + 1)))))
                 feature_index = list(map(lambda x: x[1], feature_index))
                 feature_values = [1 for _ in range(len(feature_index))]
                 y = float(row["ratings"]) / 5
                 # print("feature_indx", feature_index, "features len", len(feature_index))
                 feature_dict = {"feature_idx": feature_index, "feature_values": feature_values}
                 yield (feature_dict, y)
-        dataset = tf.data.Dataset.from_generator(gen, ({"feature_idx":tf.int64, "feature_values":tf.float32}, tf.float32),
-                                                 ({"feature_idx":tf.TensorShape([None]), "feature_values":tf.TensorShape([None])},
-                                                  tf.TensorShape([])))
-        dataset = dataset.prefetch(128).padded_batch(self.params["batch_size"],
-                                       padded_shapes=({"feature_idx": [None], "feature_values": [None]}, []))
 
+        dataset = tf.data.Dataset.from_generator(gen,
+                                                 ({"feature_idx": tf.int64, "feature_values": tf.float32}, tf.float32),
+                                                 ({"feature_idx": tf.TensorShape([None]),
+                                                   "feature_values": tf.TensorShape([None])},
+                                                  tf.TensorShape([])))
+        dataset = dataset.prefetch(self.params["batch_size"] * 10).padded_batch(self.params["batch_size"],
+                                                                                padded_shapes=({"feature_idx": [None],
+                                                                                                "feature_values": [
+                                                                                                    None]}, []))
         return dataset
+
+    def input_fn_test(self):
+        userData, itemData, rating_info, user_cols, movie_cols = get1MTrainData(self.data_path)
+        self.params["feature_size"] = len(user_cols) + len(movie_cols) - 2
+
+        def decode(row):
+            userId, itemId = int(row[0]), int(row[1])
+            userInfo, movieInfo = userData.loc[userId, :], itemData.loc[itemId, :]
+            trainData = userInfo.tolist() + movieInfo.tolist()
+            feature_index = list(filter(lambda x: x[0] == 1, zip(trainData, list(range(1, len(trainData) + 1)))))
+            feature_index = list(map(lambda x: x[1], feature_index))
+            feature_values = [1 for _ in range(len(feature_index))]
+            y = float(row["ratings"]) / 5
+            # print("feature_indx", feature_index, "features len", len(feature_index))
+            feature_dict = {"feature_idx": feature_index, "feature_values": feature_values}
+            return (feature_dict, y)
+
+        dataset = tf.data.Dataset.from_tensor_slices(rating_info).map(decode, num_parallel_calls=2)
+        dataset = dataset.prefetch(self.params["batch_size"] * 10) \
+            .padded_batch(self.params["batch_size"], padded_shapes=({"feature_idx": [None], "feature_values": [None]}, []))
+        return dataset
+
     def train(self):
 
         session_config = tf.ConfigProto(log_device_placement=True, device_count={'GPU': 0})
@@ -126,10 +164,11 @@ class FMModel(object):
                                         save_checkpoints_steps=50000).replace(session_config=session_config)
 
         fm_model = tf.estimator.Estimator(model_fn=self.fm_model_fn, model_dir="../data/model/", config=config)
-        fm_model.train(input_fn=self.train_input_fn, hooks=[tf.train.LoggingTensorHook(["first_order", "sigmoid_loss"],
-                                                                                       every_n_iter=500)])
+        fm_model.train(input_fn=self.input_fn_test, hooks=[tf.train.LoggingTensorHook(["first_order", "sigmoid_loss"],
+                                                                                      every_n_iter=500)])
+
     def test_dataset(self):
-        dataset = self.train_input_fn().make_one_shot_iterator()
+        dataset = self.input_fn_test().make_one_shot_iterator()
         next_ele = dataset.get_next()
         batch = 1
         with tf.train.MonitoredTrainingSession() as sess:
@@ -137,14 +176,16 @@ class FMModel(object):
                 value = sess.run(next_ele)
                 print("value=", value)
                 batch += 1
+
+
 def main(_):
-    params = {"embedding_size": 8, "feature_size": 0, "field_size": 1, "batch_size": 16, "learning_rate":0.001, "optimizer":"adam"}
+    params = {"embedding_size": 8, "feature_size": 0, "field_size": 1, "batch_size": 16, "learning_rate": 0.001,
+              "optimizer": "adam"}
     fm = FMModel(data_path="../data/ml-1m/", params=params)
-    # fm.test_dataset()
+    fm.test_dataset()
     fm.train()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     print(tf.__version__)
     tf.app.run()
-
-
