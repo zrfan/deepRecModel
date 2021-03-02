21d4eb77b8bfd92fcf917b9f09c222b7b740f08f
zrfan <1522193661@qq.com>
2021-03-02 09:21:54 +0800

    fx

--- models/FM_based/BiFFM.py
+++ models/FM_based/BiFFM.py
@@ -31,13 +31,14 @@ class BiFFMModel(object):
     def __init__(self, data_path, params):
         self.data_path, self.params = data_path, params
     def biffm_model_fn(self, feature, labels, mode):
+        feature_size = self.params["feature_size"]
         batch_size, learning_rate, optimizer_used = self.params["batch_size"], self.params["learning_rate"], self.params["optimizer_used"]
 
         weights = BiFFMParams(params=self.params)
         all_embeddings = weights["second_order_weights"]
         first_order_weights = weights["first_order_weights"]
         bias = weights["bias"]
-        interaction_weights_shared = weights["interaction_weights_shared"]
+        interaction_weights_shared = weights["interaction_weights_shared"]   # [emb_size, emb_size]
 
         feature_idx, feature_values, feature_fields = feature["feature_idx"], feature["feature_values"], feature["feature_fields"]
         origin_features = feature["origin_features"]
@@ -45,9 +46,13 @@ class BiFFMModel(object):
         first_order = tf.multiply(tf.nn.embedding_lookup(first_order_weights, feature_idx), feature_values, name="first_order")
         first_order = tf.reduce_sum(tf.reduce_sum(first_order, 2), 1, keep_dims=True)
 
-        feature_embeddings = tf.nn.embedding_lookup(all_embeddings, feature_idx)
-        second_order = tf.multiply(feature_embeddings, interaction_weights_shared)
+        feature_embeddings = tf.nn.embedding_lookup(all_embeddings, feature_idx)   # [batch, len, emb_size]
+        # second_order = tf.multiply(feature_embeddings, interaction_weights_shared)
 
+        second_order = tf.constant(0, dtype=tf.float32)
+        for i in range(1, feature_size+1):
+            for j in range(i+1, feature_size+1):
+                emb_i, emb_j = all_embeddings[i, :], all_embeddings[j, :]
 
 
 
